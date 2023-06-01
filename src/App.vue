7080acc263986857ca493195ff1b81e1b8a252f7
lining98 <2357490894@qq.com>
2023-06-01 17:29:59 +0800

    feat:init

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>{{ msg }}</div>
+  <div class="box">
+    <h1>{{ msg }}</h1>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref } from 'vue'
 
-const msg: string = ref("我是app根组件");
+const msg = ref('我是app根组件')
+
+const fun = ()=>
+    {
+     console.log('222');
+
+}
+fun()
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.box {
+  width: 600px;
+  height: 400px;
+  background: #000;
+  h1 {
+    color: #fff;
+  }
+}
+</style>
