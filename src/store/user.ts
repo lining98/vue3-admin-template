68dc9e941b6c981b8de5c19514fadff1a6ad3f9c
lining98 <2357490894@qq.com>
2023-06-05 10:58:43 +0800

    feat: login

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,26 +1,33 @@
 // 创建用户相关的小仓库
 import { defineStore } from 'pinia'
-// 引入数据类型
-import type { loginForm } from '@/api/user/type'
+// 引入接口
 import { reqLogin } from '@/api/user'
+// 引入数据类型
+import type { loginForm, loginResponseData } from '@/api/user/type'
+import type { UserState } from './types/type'
+// 引入操作本地存储的工具方法
+import { SET_TOKEN,GET_TOKEN } from '@/utils/token'
 
 export const useUserStore = defineStore('User', {
-  state: () => {
+  state: (): UserState => {
     return {
-      token: localStorage.getItem('token'),
+      token: GET_TOKEN(), // 用户唯一标识token
     }
   },
   getters: {},
   actions: {
     // 用户登录的方法
     async userLogin(data: loginForm) {
-      const res = await reqLogin(data)
+      const res: loginResponseData = await reqLogin(data)
       if (res.code === 200) {
-        this.token = res.data.token
+        // pinia仓库存储一下token
+        // 由于pinia|vuex存储数据其实利用js对象
+        this.token = res.data.token as string
         // 本地存储持久化存储一份
-        localStorage.setItem('token', res.data.token)
+        SET_TOKEN(res.data.token as string)
+        return 'ok'
       } else {
-        console.log(1)
+        return Promise.reject(new Error(res.data.message))
       }
     },
   },
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,16 +3,21 @@
     <el-row>
       <el-col :span="12" :xs="0"></el-col>
       <el-col :span="12" :xs="24">
-        <el-form class="login_form">
+        <el-form
+          class="login_form"
+          :model="loginForm"
+          :rules="rules"
+          ref="loginForms"
+        >
           <h1>Hello</h1>
           <h2>欢迎来到规格甄选</h2>
-          <el-form-item>
+          <el-form-item prop="username">
             <el-input
               :prefix-icon="User"
               v-model="loginForm.username"
             ></el-input>
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop="password">
             <el-input
               :prefix-icon="Lock"
               type="password"
@@ -21,7 +26,12 @@
             ></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button class="login_btn" type="primary" @click="login">
+            <el-button
+              :loading="loading"
+              class="login_btn"
+              type="primary"
+              @click="login"
+            >
               登录
             </el-button>
           </el-form-item>
@@ -33,17 +43,72 @@
 
 <script setup lang="ts">
 import { User, Lock } from '@element-plus/icons-vue'
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import { useUserStore } from '@/store/user'
+import { useRouter } from 'vue-router'
+// 引入获取当前时间的函数
+import { getTime } from '@/utils/time'
+import { ElNotification } from 'element-plus'
 const { userLogin } = useUserStore()
+
+const router = useRouter()
+
+const loginForms = ref()
+const loading = ref(false)
 
 // 收集账号与密码的数据
 const loginForm = reactive({
   username: 'admin',
   password: '111111',
 })
-const login = () => {
-  userLogin(loginForm)
+
+// 登录按钮回调
+const login = async () => {
+  // 保证全部表单校验通过再发请求
+  await loginForms.value.validate()
+
+  loading.value = true
+  try {
+    // 保证登录成功
+    await userLogin(loginForm)
+    router.push('/')
+    ElNotification({
+      type: 'success',
+      message: '欢迎回来',
+      title: `Hi,${getTime()}好`,
+    })
+
+    loading.value = false
+  } catch (error) {
+    loading.value = false
+    ElNotification.error((error as Error).message)
+  }
+}
+
+// 自定义校验规则函数
+const validatorUserName = (rule: any, value: any, callback: any) => {
+  // rule:即为校验规则对象
+  // value:即为表单元素文本的值
+  // callback() 函数 如果符合条件callback放行通过
+  //                 如果不符合callback方法，注入错误提示信息
+  if (value.length >= 5) {
+    callback()
+  } else {
+    callback(new Error('账号长度至少5位'))
+  }
+}
+const validatorPassword = (rule: any, value: any, callback: any) => {
+  if (value.length >= 6) {
+    callback()
+  } else {
+    callback(new Error('密码长度至少6位'))
+  }
+}
+
+// 定义表单校验需要配置对象
+const rules = {
+  username: [{ validator: validatorUserName, trigger: 'change' }],
+  password: [{ validator: validatorPassword, trigger: 'change' }],
 }
 </script>
 
