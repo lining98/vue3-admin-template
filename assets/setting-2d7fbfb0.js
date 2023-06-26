a4f2f3300517e47ea0fc8227dd5660c91724c291
lining98 <2357490894@qq.com>
2023-06-26 16:23:39 +0800

    deploy

---
+++ assets/setting-2d7fbfb0.js
@@ -0,0 +1,1 @@
+import{M as e}from"./index-e4bf949e.js";const r=e("Setting",{state:()=>({fold:!1,refresh:!1})});export{r as u};
--- assets/setting-353a3958.js
+++
@@ -1,1 +0,0 @@
-import{M as e}from"./index-a15cff6e.js";const r=e("Setting",{state:()=>({fold:!1,refresh:!1})});export{r as u};
